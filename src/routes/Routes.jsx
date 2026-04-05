1688938e804d153314c82cb593c4f0ee18048ec5
Shubham Samu <user@example.com>
2026-04-05 15:21:33 +0530

    Fix: Restore Config import for proper routing

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -5,16 +5,19 @@ import Home from "../pages/Home";
 import Catalog from "../pages/Catalog";
 import Detail from "../pages/detail/Detail";
 
+import * as Config from "../constants/Config";
+
 const Routes = () => {
   return (
     <ReactRoutes>
       <Route
-        path={`/:category/search/:keyword`}
+        path={`/${Config.HOME_PAGE}/:category/search/:keyword`}
         element={<Catalog />}
       />
-      <Route path={`/:category/:id`} element={<Detail />} />
-      <Route path={`/:category`} element={<Catalog />} />
-      <Route path={`/`} element={<Home />} />
+      <Route path={`/${Config.HOME_PAGE}/:category/:id`} element={<Detail />} />
+      <Route path={`/${Config.HOME_PAGE}/:category`} element={<Catalog />} />
+      <Route path={`/${Config.HOME_PAGE}`} element={<Home />} />
+      <Route path="/" element={<Home />} />
     </ReactRoutes>
   );
 };
